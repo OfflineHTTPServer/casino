7a2c82caa719a75278de72911a8a5ec4ffc73677
OfflineHTTPServer <runninghttpserver@gmail.com>
2025-05-25 16:24:04 +0200

    Add Navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,83 @@
+"use client";
+import {
+    Navbar,
+    NavBody,
+    NavItems,
+    MobileNav,
+    NavbarLogo,
+    NavbarButton,
+    MobileNavHeader,
+    MobileNavToggle,
+    MobileNavMenu,
+} from "@/components/ui/resizable-navbar";
+import { useState } from "react";
+
+export default function SiteNavbar() {
+
+    const navItems = [
+        {
+            name: "Features",
+            link: "/features",
+        },
+        {
+            name: "About Us",
+            link: "/about-us",
+        },
+        {
+            name: "Contact",
+            link: "/contact",
+        },
+    ];
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    return (
+        <div className="w-full fixed top-4">
+            <Navbar>
+                <NavBody>
+                    <NavbarLogo />
+                    <NavItems items={navItems} />
+                    <div className="flex items-center gap-4">
+                        <NavbarButton variant="primary">Login</NavbarButton>
+                    </div>
+                </NavBody>
+
+                <MobileNav>
+                    <MobileNavHeader>
+                        <NavbarLogo />
+                        <MobileNavToggle
+                            isOpen={isMobileMenuOpen}
+                            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                        />
+                    </MobileNavHeader>
+
+                    <MobileNavMenu
+                        isOpen={isMobileMenuOpen}
+                        onClose={() => setIsMobileMenuOpen(false)}
+                    >
+                        {navItems.map((item, idx) => (
+                            <a
+                                key={`mobile-link-${idx}`}
+                                href={item.link}
+                                onClick={() => setIsMobileMenuOpen(false)}
+                                className="relative text-neutral-600 dark:text-neutral-300"
+                            >
+                                <span className="block">{item.name}</span>
+                            </a>
+                        ))}
+                        <div className="flex w-full flex-col gap-4">
+                            <NavbarButton
+                                onClick={() => setIsMobileMenuOpen(false)}
+                                variant="primary"
+                                className="w-full"
+                            >
+                                Login
+                            </NavbarButton>
+                        </div>
+                    </MobileNavMenu>
+                </MobileNav>
+            </Navbar>
+
+        </div>
+    );
+}
